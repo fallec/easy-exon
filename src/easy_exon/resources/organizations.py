baf9dca3ec0f974beb10be583c3cee2dfc069258
Грехов Андрей Валерьевич <a.grekhov@ano.local>
2025-07-04 08:38:13 +0300

    Fix OrganizationResource

--- src/easy_exon/resources/organizations.py
+++ src/easy_exon/resources/organizations.py
@@ -6,8 +6,8 @@ class OrganizationsResource:
     def __init__(self, client):
         self._client = client
 
-    def list(self, organization_id: str) -> List[OrganizationModel]:
-        data = self._client.post("/api/org-service/organizations/all", json=[organization_id])
+    def list(self, organization_id: list[str]) -> List[OrganizationModel]:
+        data = self._client.post("/api/org-service/organizations/all", json=[o for o in organization_id])
         return [OrganizationModel.model_validate(item) for item in data]
 
     def json(self, organization_id: str) -> dict:
